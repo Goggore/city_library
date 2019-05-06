be034a1c215297c9222ac642fca12326bb5e69a3
Goggore <42915245+Goggore@users.noreply.github.com>
2019-05-05 22:20:50 -0400

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,42 @@
+$("#readerMenu").click(function () {
+    $("#login-form").hide()
+    $("#reader-form").show()
+
+})
+$("#adminMenu").click(function () {
+    $("#reader-form").hide()
+    $("#login-form").show()
+})
+$("#reader-form").on('submit',function(event) {
+    event.preventDefault()
+    var cardNO = event.target.CardNO.value
+    login(cardNO,function (Res) {
+        var res = JSON.parse(Res)
+        if(res.error){
+            alert(res.error)
+        }
+        else {
+            alert("Welcome back,"+res.name)
+            window.location.href = "home.php"
+        }
+    })
+})
+function adminCheck() {
+    event.preventDefault()
+    if(document.getElementById("IDInput").value == 123 && document.getElementById("PasswordInput").value == 123){
+        $.cookie("logged-in", "admin", { path: '/', exipires: 1000 });
+        $(location).attr("href","home.php")
+    }else{
+        alert("Invalid account information")
+    }
+}
+function login(cardNO, response) {
+    $.ajax("api/login.php",{
+        method:"post",
+        success:response,
+        datatype: "json",
+        data: {
+            cardNO:cardNO
+        }
+    })
+}

7e7ceb7c61f37031b24aa2c6eed335b77427806f
Goggore <42915245+Goggore@users.noreply.github.com>
2019-05-05 22:24:24 -0400

    Create login.php

---
+++ api/login.php
@@ -0,0 +1,23 @@
+<?php
+include_once('dbcheck.php');
+$res = null;
+if(isset($_POST['cardNO'])) {
+    $cardNO = $_POST['cardNO'];
+    $sql = "select * from READER where READERID = $cardNO";
+    $result = $conn->query($sql);
+    if($result->num_rows > 0){
+        if($row = $result->fetch_assoc()){
+            $res->name = $row["RNAME"];
+            setcookie("logged_in",$row["READERID"],time()+3000,'/');
+        }
+
+    }
+    else{
+        $res->error = "Invalid reader ID. Please try again.";
+    }
+}
+if(!$res){
+    $res->error = "Unknown error occurred. Please try again later.";
+}
+echo json_encode($res);
+?>

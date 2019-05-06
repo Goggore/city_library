60cbf0f9202d670f9f31f8da0e0fec1a40e86a12
Goggore <42915245+Goggore@users.noreply.github.com>
2019-05-06 12:11:01 -0400

    Create addReader.php

---
+++ api/addReader.php
@@ -0,0 +1,29 @@
+<?php
+include_once('dbcheck.php');
+$res = null;
+if(isset($_POST['name'],$_POST['address'],$_POST['type'])
+    &&$_POST['name']!=""&&$_POST['address']!=""&&$_POST['type']!="") {
+    $name = $_POST['name'];
+    $address = $_POST['address'];
+    $type = $_POST['type'];
+    $sql = "INSERT INTO READER(RTYPE, RNAME, ADDRESS)
+                VALUES('$type','$name','$address') ";
+    if(isset($_POST['RID'])&&$_POST['RID']!=""){
+        $RID =$_POST['RID'];
+        $sql = "INSERT INTO READER(READERID, RTYPE, RNAME, ADDRESS)
+                VALUES('$RID','$type','$name','$address') ";
+    }else{
+        $RID = "";
+    }
+    if ($conn->query($sql) === TRUE) {
+        $res->msg =  $RID;
+    } else {
+        $res->error =  "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+else{
+    $res->error = "Please fill required fields";
+}
+
+echo json_encode($res);
+?>

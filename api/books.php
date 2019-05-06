c3e2f3b8c00774e8651a6f7599d13d4cafa80a6e
Goggore <42915245+Goggore@users.noreply.github.com>
2019-05-06 12:11:26 -0400

    Create books.php

---
+++ api/books.php
@@ -0,0 +1,24 @@
+<?php
+include_once('dbcheck.php');
+$res = null;
+if(isset($_POST['libID'])) {
+    $libID = $_POST['libID'];
+//    $sql = "SELECT DOCID, TITLE FROM (SELECT TOP 10 FROM (SELECT DOCID, TITLE, COUNT(*) FROM DOCUMENT AS D, BORROWS AS B WHERE D.DOCID = B.DOCID AND LIBID = '$libID' GROUP BY DOCID) ORDER BY COUNT(BORNUMBER) );";
+    $sql = "SELECT * FROM DOCUMENT";
+    $result = $conn->query($sql);
+    if($result->num_rows > 0){
+        $res->msg = "";
+        $i = 1;
+        while($row = $result->fetch_assoc()){
+            $res->msg = $res->msg.$i.". "."Document ID:".$row['DOCID'].", Title:".$row['TITLE']."\n";
+            $i++;
+        }
+    }else{
+        $res->error = "No result";
+    }
+}
+if(!$res){
+    $res->error = "Unknown error occurred. Please try again later.";
+}
+echo json_encode($res);
+$conn->close();

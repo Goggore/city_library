a02b8342accb5f38a5f9b5cd2916aeda0607fbe9
Goggore <42915245+Goggore@users.noreply.github.com>
2019-05-06 12:14:42 -0400

    Create addCopy.js

---
+++ js/addCopy.js
@@ -0,0 +1,30 @@
+$("#addCopy-form").on('submit',function(event) {
+    event.preventDefault()
+    var DocID = event.target.DocID.value
+    var CopyNo = event.target.CopyNo.value
+    var LibID = event.target.LibID.value
+    var Position = event.target.Position.value
+    addReader(DocID,CopyNo,LibID,Position,function (Res) {
+        var res = JSON.parse(Res)
+        if(res.error){
+            alert(res.error)
+        }
+        else {
+            alert(res.msg)
+        }
+    })
+})
+
+function addReader(DocID,CopyNo,LibID,Position, response) {
+    $.ajax("api/addCopy.php",{
+        method:"post",
+        success:response,
+        datatype: "json",
+        data: {
+            DocID:DocID,
+            CopyNo:CopyNo,
+            LibID:LibID,
+            Position:Position
+        }
+    })
+}

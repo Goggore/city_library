d7ff8fbc07c3ce22230b9f555c12a222cdf97c80
Goggore <42915245+Goggore@users.noreply.github.com>
2019-05-07 18:24:33 -0400

    Update fine_form.php

--- api/fine_form.php
+++ api/fine_form.php
@@ -2,6 +2,7 @@
 include_once('dbcheck.php');
 $res = null;
 $seconds = time();
+$readerID = $_COOKIE['logged_in'];
 $date = date('Y-m-d', $seconds);
 if(isset($_POST['copyno'],$_POST['docid'])
     &&$_POST['copyno']!=""&&$_POST['docid']!="") {
@@ -9,13 +10,15 @@ if(isset($_POST['copyno'],$_POST['docid'])
     $copyno = $_POST['copyno'];
     $sql = "SELECT DATEDIFF('$date', BDTIME) * 20
           FROM BORROWS
-          WHERE RDTIME IS NULL AND DATEDIFF('$date', BDTIME) > 20	AND DOCID = $docid AND COPYNO = $copyno
+          WHERE RDTIME IS NULL AND DATEDIFF('$date', BDTIME) > 20	AND DOCID = $docid AND COPYNO = $copyno AND READERID = $readerID
            ";
     $result = $conn->query($sql);
     if($result->num_rows > 0){
-        if($row = $result->fetch_array()){
-            $res->msg =  $row[0]." cents";
+        $sum = 0;
+        while($row = $result->fetch_array()){
+            $sum += (int)$row[0];
         }
+        $res->msg =  $sum." cents";
     }else{
         $res->error = "No result";
     }
